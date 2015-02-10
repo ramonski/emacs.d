f74e75ccfdd366bb32e3764534fea51941442f91
Ramon Bartl <ramon.bartl@nexiles.com>
2015-02-10 18:51:11 +0100

    updated key bindings

--- init/init-evil.el
+++ init/init-evil.el
@@ -49,11 +49,18 @@
 ; Navigation
 (require 'neotree)
 (add-hook 'neotree-mode-hook
-          (lambda ()
-            (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
-            (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
-            (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
-            (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+  (lambda ()
+    (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-stretch-toggle)
+    (define-key evil-normal-state-local-map (kbd "q")   'neotree-hide)
+    (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+    (define-key evil-normal-state-local-map (kbd "|")   'neotree-enter-vertical-split)
+    (define-key evil-normal-state-local-map (kbd "-")   'neotree-enter-horizontal-split)
+    (define-key evil-normal-state-local-map (kbd "C")   'neotree-change-root)
+    (define-key evil-normal-state-local-map (kbd "r")   'neotree-refresh)
+    (define-key evil-normal-state-local-map (kbd "u")   'neotree-select-up-node)
+    (define-key evil-normal-state-local-map (kbd ".")   'neotree-hidden-file-toggle)
+  )
+)
 
 ; Key Bindings in Insert Mode
 (evil-define-key 'insert global-map
